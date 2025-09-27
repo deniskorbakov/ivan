022ffe0a4438973c301ca05804f1b7e4a56aafa5
Denis <korbakovd@gmail.com>
2025-09-27 10:30:35 +0300

    feat: add gh action

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 # use linter for formatted code
 lint:
-	docker run -t --rm -v $$(pwd):/app -w /app golangci/golangci-lint:v2.1.6 golangci-lint run
+	docker run -t --rm -v $$(pwd):/app -w /app golangci/golangci-lint:v2.5.0 golangci-lint run
 
 build:
 	go mod vendor
--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -1,13 +1,21 @@
 package storage
 
-import "os"
+import (
+	"log"
+	"os"
+)
 
 func TempDir() (string, error) {
 	repDir, err := os.MkdirTemp("", "dir")
 	if err != nil {
 		return "", err
 	}
-	defer os.RemoveAll(repDir)
+	defer func(path string) {
+		err := os.RemoveAll(path)
+		if err != nil {
+			log.Printf("Failed to remove temporary directory: %v", err)
+		}
+	}(repDir)
 
 	return repDir, nil
 }
